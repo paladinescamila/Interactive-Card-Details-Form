ce7e542fc1584b4c40f62d1f5abc14ed3273ec56
Camila Paladines <camilapaladines27@gmail.com>
2022-08-29 02:48:43 -0500

    Fix warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/App.scss';
 import DesktopBackground from './assets/bg-main-desktop.png';
-import MobileBackground from './assets/bg-main-mobile.png';
+// import MobileBackground from './assets/bg-main-mobile.png';
 import FrontCard from './components/FrontCard/FrontCard';
 import BackCard from './components/BackCard/BackCard';
 import Form from './components/Form/Form';
